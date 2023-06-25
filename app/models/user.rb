5bac24a9bb04a61cfbe85819b8ed6647714059ba
yukke3duck <31387999+yukke3duck@users.noreply.github.com>
2023-06-25 14:33:10 +0900

    add: Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i
@@ -11,5 +13,31 @@ class User < ApplicationRecord
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost:)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+    remember_digest
+  end
+
+  # セッションハイジャック防止のためにセッショントークンを返す
+  # この記憶ダイジェストを裁量しているのは単に利便性のため
+  def session_token
+    remember_digest || remember
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
-class UsersLoginTest < ActionDispatch::IntegrationTest
+class UsersLogin < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
 end
 
-class InvalidPasswordTest < UsersLoginTest
+class InvalidPasswordTest < UsersLogin
   test 'login path' do
     get login_path
     assert_template 'sessions/new'
@@ -24,7 +24,7 @@ class InvalidPasswordTest < UsersLoginTest
   end
 end
 
-class ValidLogin < UsersLoginTest
+class ValidLogin < UsersLogin
   def setup
     super
     post login_path, params: { session: { email: @user.email,
@@ -66,5 +66,25 @@ class LogoutTest < Logout
     assert_select 'a[href=?]', login_path
     assert_select 'a[href=?]', logout_path,       count: 0
     assert_select 'a[href=?]', user_path(@user),  count: 0
+  end
+
+  test 'should still work after logout in second window' do
+    delete logout_path
+    assert_redirected_to root_url
+  end
+end
+
+class RemeberingTest < UsersLogin
+  test 'login with remembering' do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
+  end
+
+  test 'login without remembering' do
+    # cookiesを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    # cookiesが削除されることを検証
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
   end
 end
