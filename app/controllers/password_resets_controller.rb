e05bb5c9fc1dd61b669a294a3e512e6ae5793fe0
yukke3duck <31387999+yukke3duck@users.noreply.github.com>
2023-07-12 09:41:38 +0900

    Add password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,8 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: %i[edit update]
+  before_action :valid_user, only: %i[edit update]
+  before_action :check_expiration, only: %i[edit update]
+
   def new; end
 
   def create
@@ -15,4 +19,46 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit; end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render 'edit', status: :unprocessable_entity
+    elsif @user.update(user_params)
+      forget(@user)
+      reset_session
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = 'Password has been reset.'
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  # beforeフィルタ
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  def valid_user
+    unless @user && @user.activated? &&
+           @user.authenticated?(:reset, params[:id])
+      redirect_to root_url
+    end
+  end
+
+  def check_expiration
+    return unless @user.password_reset_expired?
+
+    flash[:danger] = 'Password reset has expired.'
+    redirect_to new_password_reset_url
+  end
 end
