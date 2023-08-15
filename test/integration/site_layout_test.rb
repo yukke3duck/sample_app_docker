5885d08e0e54066e292f6c2fc8a447637c20ce5c
yukke3duck <31387999+yukke3duck@users.noreply.github.com>
2023-08-15 16:00:12 +0900

    add micropost form on home page when logged in

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -28,14 +28,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', help_path
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', contact_path
-    assert_select 'a[href=?]', signup_path
     assert_select 'a[href=?]', users_path
     assert_select 'a[href=?]', user_path(@user)
     assert_select 'a[href=?]', edit_user_path(@user)
     assert_select 'a[href=?]', logout_path
     get contact_path
     assert_select 'title', full_title('Contact')
-    get signup_path
-    assert_select 'title', full_title('Sign up')
   end
 end
