1dba7aa688b515dde584d0837ce95fb33678754d
yukke3duck <31387999+yukke3duck@users.noreply.github.com>
2023-08-27 06:05:17 +0900

    add validattion for micropost with image on frontend

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,7 +4,7 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  validates :image, content_type: { in: %w[iamge/jpag image/gif image/png],
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
                                     message: 'must be a valid image format' },
                     size: { less_than: 5.megabytes,
                             message: 'should be less than 5MB' }
