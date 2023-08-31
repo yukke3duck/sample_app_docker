78c72b898e3bc58480943d8421337791a882e20f
yukke3duck <31387999+yukke3duck@users.noreply.github.com>
2023-09-01 07:00:20 +0900

    update query for feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,10 +80,10 @@ class User < ApplicationRecord
 
   # ユーザーのステータスフィードを返す
   def feed
-    following_ids = "SELECT followed_id FROM relationships
-                      WHERE follower_id = :user_id"
-    Micropost.where("user_id IN (#{following_ids})
-                      OR user_id = :user_id", user_id: id)
+    part_of_feed = "relationships.follower_id = :id
+                    or microposts.user_id = :id"
+    Micropost.left_joins(user: :followers)
+             .where(part_of_feed, { id: }).distinct
              .includes(:user, image_attachment: :blob)
   end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -107,6 +107,7 @@ class UserTest < ActiveSupport::TestCase
     # フォロワーがいるユーザー自身の投稿を確認
     michael.microposts.each do |post_self|
       assert michael.feed.include?(post_self)
+      assert_equal michael.feed.distinct, michael.feed
     end
     # フォローしていないユーザーの投稿を確認
     archer.microposts.each do |post_unfollowed|
