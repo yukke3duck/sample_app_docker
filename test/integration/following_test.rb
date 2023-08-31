6944aaee1fe96d5d06588af22566db7a0b5d1870
yukke3duck <31387999+yukke3duck@users.noreply.github.com>
2023-09-01 06:38:16 +0900

    add integration test for status feed

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -69,3 +69,12 @@ class UnfollowTest < Unfollow
     end
   end
 end
+
+class FollowPagesTest < Following
+  test 'feed on Home page' do
+    get root_path
+    @user.feed.paginate(page: 1).each do |micropost|
+      assert_match CGI.escapeHTML(micropost.content), response.body
+    end
+  end
+end
